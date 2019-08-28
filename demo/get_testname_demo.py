5f817b023c6c6fcebd796356beed10501fa9b564
Dilip Chauhan Mac <dilipchauhan20@ymail.com>
2019-08-28 22:40:01 +0530

    Adding allure reports and demo folder

---
+++ demo/get_testname_demo.py
@@ -0,0 +1,17 @@
+import inspect
+# functions
+def whoami():
+    return inspect.stack()[1][3]
+def whosdaddy():
+    return inspect.stack()[2][3]
+def foo():
+    print("hello, I'm %s, daddy is %s" % (whoami(), whosdaddy()))
+    bar()
+def bar():
+    print("hello, I'm %s, daddy is %s" % (whoami(), whosdaddy()))
+
+johny = bar
+# call them!
+foo()
+bar()
+johny()
